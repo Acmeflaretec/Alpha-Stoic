c8c495ef858095ec1910ec46f802cadb86064727
abdusamad8575 <samadpts786313@gmail.com>
2024-06-11 18:07:55 +0530

    new updetions

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'], 
+  origin: [process.env.CLIENT,process.env.ADMIN], 
   credentials: true,
 }));
 
