78428f099f1cfd4c889b4f125869b4195989e883
abdusamad8575 <samadpts786313@gmail.com>
2024-06-11 11:00:44 +0530

    team updations

--- client/src/components/About/Team.tsx
+++ client/src/components/About/Team.tsx
@@ -1,5 +1,4 @@
 import SectionTitle from "../Common/SectionTitle";
-// import TeamMember from "./TeamMember"; 
 
 const Team = () => {
   // Sample team member data
@@ -11,31 +10,31 @@ const Team = () => {
       image: "/images/team/Untitled presentation.pdf-image-001.jpg",
     },
     {
-      id: 3,
+      id: 2,
       name: "Jinesh Kumar",
       position: "CFO",
       image: "/images/team/Untitled presentation.pdf-image-002.jpg",
     },
     {
-      id: 2,
+      id: 3,
       name: "Akash Aramana",
       position: "Chief strategist",
       image: "/images/team/Untitled presentation.pdf-image-003.jpg",
     },
     {
-      id: 1,
+      id: 4,
       name: "Nikhil NK",
       position: "Head of Research",
       image: "/images/team/Untitled presentation.pdf-image-004.jpg",
     },
     {
-      id: 3,
+      id: 5,
       name: "Gautham Krishna",
       position: "Research Analyst",
       image: "/images/team/Untitled presentation.pdf-image-005.jpg",
     },
     {
-      id: 2,
+      id: 6,
       name: "Arjun",
       position: "Research Analyst",
       image: "/images/team/Untitled presentation.pdf-image-006.jpg",
@@ -52,8 +51,8 @@ const Team = () => {
         />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {teamMembers.map((member) => (
-            <div className="flex flex-col items-center bg-gray-light dark:bg-bg-color-dark">
-              <img src={member.image} alt={member.name} className="mb-4" />
+            <div key={member.id} className="flex flex-col items-center bg-gray-light dark:bg-bg-color-dark p-4 rounded-lg shadow-md">
+              <img src={member.image} alt={member.name} className="mb-4 rounded-full" />
               <h3 className="text-lg font-semibold mb-2">{member.name}</h3>
               <p className="text-sm text-gray-600 mb-5">{member.position}</p>
             </div>
