a5fff18f619536703728a65be09fa2527702dd13
abdusamad8575 <samadpts786313@gmail.com>
2024-06-11 21:19:53 +0530

    api updation completed

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: [process.env.CLIENT,process.env.ADMIN], 
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
