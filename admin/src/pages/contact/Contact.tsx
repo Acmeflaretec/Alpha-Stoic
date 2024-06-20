c62fcd9b546fa90350c4b3db140dcd406fa09f8b
Abhijith Babu <abijithb2002@gmail.com>
2024-06-20 16:17:15 +0530

    update:contact-page message field added

--- admin/src/pages/contact/Contact.tsx
+++ admin/src/pages/contact/Contact.tsx
@@ -1,3 +1,4 @@
+import { Fade, Tooltip } from "@mui/material";
 import DataTable from "../../components/contact/DataTable";
 import "./users.scss";
 import { GridColDef } from "@mui/x-data-grid";
@@ -9,9 +10,21 @@ const Contact = () => {
 
   const columns: GridColDef[] = [
     { field: "_id", headerName: "ID", width: 120 },
-    { field: "name", type: "string", headerName: "Name", width: 300 },
-    { field: "email", type: "string", headerName: "Email", width: 300 },
+    { field: "name", type: "string", headerName: "Name", width: 250 },
+    { field: "email", type: "string", headerName: "Email", width: 250 },
     { field: "contactNumber", type: "string", headerName: "Phone", width: 200 },
+    {
+      field: "message", type: "string", headerName: "Message", width: 200,
+      renderCell: (params) => (
+        <Tooltip
+          title={params.value || ''} placement="bottom-start"
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 600 }}
+        >
+          <span>{params.value}</span>
+        </Tooltip>
+      )
+    },
     { field: "verified", headerName: "Verified", width: 150, type: "boolean" },
   ];
 
