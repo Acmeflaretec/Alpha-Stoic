e93bef455bbe2943223837b24832e33f3b0552fb
abdusamad8575 <samadpts786313@gmail.com>
2024-07-10 13:29:16 +0530

    t&C added

--- client/src/app/Terms-and-Conditions/page.tsx
+++ client/src/app/Terms-and-Conditions/page.tsx
@@ -1,112 +1,68 @@
 import React from 'react';
 
-const Cancellation = () => {
+const Terms = () => {
     return (
         <div className="container" style={{ marginTop: '100px' }}>
             <h3 className="text-center" style={{ fontWeight: "500", fontSize: '25px' }}>Terms and Conditions</h3>
             <div>10-07-2024</div>
             <h4>Introduction</h4>
             <p>Welcome to Alpha Stoic. These Terms and Conditions govern your use of our website located at www.alphastoic.in. By accessing or using our website, you agree to comply with and be bound by these Terms and Conditions. If you do not agree with these Terms and Conditions, please refrain from using our website.</p>
-            <h4>Definitions
-            </h4>
-            <p>"Client", "You", and "Your" refer to you, the individual accessing our website and accepting the Company’s terms and conditions.
-            </p>
-            <p>"The Company", "Ourselves", "We", "Our", and "Us" refer to Alpha Stoic.
-            </p>
-            <p>"Party", "Parties", or "Us" refer to both the Client and ourselves.
-            </p>
-            <h5>Use of Services
-            </h5>
-            <h6>Eligibility
-            </h6>
-            <p>To use our services, you must be at least 15 years old.
-            </p>
-            <h6>Registration
-            </h6>
-            <p>To access certain features of our website, you may be required to register and create an account. You agree to provide accurate and complete information during the registration process and to keep this information up to date</p>
+            <h4>Definitions</h4>
+            <p>&quot;Client&quot;, &quot;You&quot;, and &quot;Your&quot; refer to you, the individual accessing our website and accepting the Company&rsquo;s terms and conditions.</p>
+            <p>&quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot;, and &quot;Us&quot; refer to Alpha Stoic.</p>
+            <p>&quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot; refer to both the Client and ourselves.</p>
+            <h5>Use of Services</h5>
+            <h6>Eligibility</h6>
+            <p>To use our services, you must be at least 15 years old.</p>
+            <h6>Registration</h6>
+            <p>To access certain features of our website, you may be required to register and create an account. You agree to provide accurate and complete information during the registration process and to keep this information up to date.</p>
             <h5>User Responsibility</h5>
-            <p>You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account.
-            </p>
+            <p>You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account.</p>
             <ul>
-                <li>Financial Services
-                </li>
-                <li>Advisory Services
-                </li>
-                <li>The financial advice provided on our website is for informational purposes only and we are not a SEBI Registered company.
-                </li>
+                <li>Financial Services</li>
+                <li>Advisory Services</li>
+                <li>The financial advice provided on our website is for informational purposes only and we are not a SEBI Registered company.</li>
             </ul>
-            <h6>Investment Risk
-            </h6>
-            <p>All investments are subject to risks, including the possible loss of capital. We do not guarantee the performance of any investment, as all the information is for educational purposes only.
-            </p>
-            <h6>Transactions
-            </h6>
-            <p>All transactions on our platform are subject to compliance with Indian laws and regulations. We reserve the right to refuse or cancel any transaction that does not comply with these laws.
-            </p>
+            <h6>Investment Risk</h6>
+            <p>All investments are subject to risks, including the possible loss of capital. We do not guarantee the performance of any investment, as all the information is for educational purposes only.</p>
+            <h6>Transactions</h6>
+            <p>All transactions on our platform are subject to compliance with Indian laws and regulations. We reserve the right to refuse or cancel any transaction that does not comply with these laws.</p>
             <h6>Intellectual Property</h6>
-            <p>Unless otherwise stated, Alpha Stoic and/or its licensors own the intellectual property rights for all material on this website. All intellectual property rights are reserved. You may access this material for your personal use, subject to the restrictions set out in these Terms and Conditions.
-            </p>
-            <h4>You must not:
-            </h4>
+            <p>Unless otherwise stated, Alpha Stoic and/or its licensors own the intellectual property rights for all material on this website. All intellectual property rights are reserved. You may access this material for your personal use, subject to the restrictions set out in these Terms and Conditions.</p>
+            <h4>You must not:</h4>
             <ul>
-                <li>Republish material from Alpha Stoic
-                </li>
-                <li>Sell, rent, or sub-license material from Alpha Stoic
-                </li>
+                <li>Republish material from Alpha Stoic</li>
+                <li>Sell, rent, or sub-license material from Alpha Stoic</li>
                 <li>Reproduce, duplicate, or copy material from Alpha Stoic</li>
-                <li>
-                    Redistribute content from Alpha Stoic
-                </li>
+                <li>Redistribute content from Alpha Stoic</li>
             </ul>
-            <h5>User Content
-            </h5>
-            <h6>Content Responsibility
-            </h6>
-            <p>You are responsible for the content that you post on our website. You must not post any content that is offensive, defamatory or infringes on any intellectual property rights.
-            </p>
-            <h5>Content License
-            </h5>
-            <p>By posting content on our website, you grant us a non-exclusive, royalty-free, perpetual, and worldwide license to use, reproduce, modify, and distribute your content.
-            </p>
-            <h5>Limitation of Liability
-            </h5>
-            <p>In no event shall Alpha Stoic, nor any of its officers, directors, and employees, be held liable for anything arising out of or in any way connected with your use of this website, whether such liability is under contract. Alpha Stoic, including its officers, directors, and employees, shall not be held liable for any indirect, consequential, or special liability arising out of or in any way related to your use of this website.
-            </p>
-            <h5>Indemnification
-            </h5>
-            <p>You hereby indemnify to the fullest extent Alpha Stoic from and against any liabilities, costs, demands, causes of action, damages, and expenses arising in any way related to your breach of any of the provisions of these Terms and Conditions.
-            </p>
-            <h5>Termination
-            </h5>
-            <p>We may terminate or suspend access to our website immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach these Terms and Conditions. All provisions of these Terms and Conditions which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.            </p>
-            <h5>Governing Law
-            </h5>
-            <p>These Terms and Conditions are governed by and construed following the laws of India, and you irrevocably submit to the exclusive jurisdiction of the courts in that jurisdiction.
-            </p>
-            <h5>Changes to Terms
-            </h5>
-            <p>We reserve the right to amend these Terms and Conditions at any time. Your continued use of the website following any changes signifies your acceptance of such changes.
-            </p>
-            <h5>Contact Information
-            </h5>
+            <h5>User Content</h5>
+            <h6>Content Responsibility</h6>
+            <p>You are responsible for the content that you post on our website. You must not post any content that is offensive, defamatory, or infringes on any intellectual property rights.</p>
+            <h5>Content License</h5>
+            <p>By posting content on our website, you grant us a non-exclusive, royalty-free, perpetual, and worldwide license to use, reproduce, modify, and distribute your content.</p>
+            <h5>Limitation of Liability</h5>
+            <p>In no event shall Alpha Stoic, nor any of its officers, directors, and employees, be held liable for anything arising out of or in any way connected with your use of this website, whether such liability is under contract. Alpha Stoic, including its officers, directors, and employees, shall not be held liable for any indirect, consequential, or special liability arising out of or in any way related to your use of this website.</p>
+            <h5>Indemnification</h5>
+            <p>You hereby indemnify to the fullest extent Alpha Stoic from and against any liabilities, costs, demands, causes of action, damages, and expenses arising in any way related to your breach of any of the provisions of these Terms and Conditions.</p>
+            <h5>Termination</h5>
+            <p>We may terminate or suspend access to our website immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach these Terms and Conditions. All provisions of these Terms and Conditions which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.</p>
+            <h5>Governing Law</h5>
+            <p>These Terms and Conditions are governed by and construed following the laws of India, and you irrevocably submit to the exclusive jurisdiction of the courts in that jurisdiction.</p>
+            <h5>Changes to Terms</h5>
+            <p>We reserve the right to amend these Terms and Conditions at any time. Your continued use of the website following any changes signifies your acceptance of such changes.</p>
+            <h5>Contact Information</h5>
             <p>If you have any questions about these Terms and Conditions, please contact us at:</p>
-            <h5>Limitation
-            </h5>
+            <h5>Limitation</h5>
             <ul>
-                <li>Alpha Stoic Education Hub
-</li>
-                <li>2nd floor, Aysha Tower, TB Road, Kanhangad, Kerala 671315
-</li>
-                <li>alphastoichub@gmail.com
-</li>
-                <li>9567508882,967464474
-                </li>
+                <li>Alpha Stoic Education Hub</li>
+                <li>2nd floor, Aysha Tower, TB Road, Kanhangad, Kerala 671315</li>
+                <li>alphastoichub@gmail.com</li>
+                <li>9567508882, 967464474</li>
             </ul>
-            <p>By using our website, you agree to comply with and be bound by these Terms and Conditions.</p>
-
-
+            <p>By using our website, you agree to comply with and be bound by these Terms and Conditions.</p>
         </div>
     );
 }
 
-export default Cancellation;
+export default Terms;
